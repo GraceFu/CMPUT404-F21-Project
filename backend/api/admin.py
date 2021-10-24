aea7ef18d7e297aba7f8087edc75245192c4f491
zhining_andi <zhininghjl@gmail.com>
2021-10-23 21:31:09 -0600

    constructure structure (#25)
    * add gitignore, build potential folder structure for the project

---
+++ backend/api/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models.profiles import Profile
+
+# Register your models here.
+
+
+admin.site.register(Profile)
---
+++ backend/api/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+import django.contrib.auth.models
+from utils import generate_id
+
+# Create your models here.
+
+class Profile(models.Model):
