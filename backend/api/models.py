21b37aa4e713a762828ae79f69f1fa8ea0e95d1f
zhining_andi <zhininghjl@gmail.com>
2021-10-25 23:46:42 -0600

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,11 +7,9 @@ from django.db.models.fields import related
 from django.conf import settings
 from backend.Social_network.settings import HOSTNAME
 from utils import generate_id
-import uuid
 from django import forms
 
 # Create your models here.
-##signup and friends not implemented yet
 
 visibility_choices = [
     ('public', 'Public'),
@@ -20,13 +18,12 @@ visibility_choices = [
 
 ######### Author #########
 class Author(models.Model):
-        id = models.CharField(primary_key=True, default=uuid.uuid4, max_length=100, unique=True, editable=False)
-        user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
-        tokens = models.CharField(max_length=100,default="1234")
-        displayName = models.CharField(max_length=100)
-        host = models.CharField(default=HOSTNAME, max_length=500)
-        url = models.URLField(null=True, blank=True)
-        github = models.URLField(null=True, blank=True)
+    author_id = models.UUIDField(primary_key=True, default=generate_id, editable=False, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    display_name = models.CharField(max_length=100)
+    host = models.CharField(default=HOSTNAME, max_length=500)
+    url = models.URLField(null=True, blank=True)
+    github = models.URLField(null=True, blank=True)
 
 
 ######### Follow #########
@@ -39,6 +36,7 @@ class Follow(models.Model):
     request_acceptance = models.BooleanField(default=False)
     friend_status = models.BooleanField(default=False)
 
+    
 ######### Inbox #########
 class Inbox(models.Model):
     # https://docs.djangoproject.com/en/3.2/ref/models/fields/#choices
@@ -52,75 +50,69 @@ class Inbox(models.Model):
     published = models.DateTimeField(auto_now_add=True)
 
 
-
 ######### Post #########
 class Post(models.Model):
-        post_id = models.CharField(primary_key=True, default=uuid.uuid4, max_length=100, unique=True, editable=False)
-        author_id = models.ForeignKey(Author, on_delete=models.CASCADE,)
-        title = models.CharField(max_length=100)
-        origin_post = models.URLField()
-        source = models.URLField()
-        description = models.CharField(max_length=200)
-        content = models.CharField(max_length=500, null=True, blank=True)
-        contentType = models.CharField(max_length=50, default="text/html", blank=False, null=False)
-        image_content = models.TextField(null=True, blank=True)
-        categories_id = models.JSONField()
-        published_date = models.DateTimeField(default=timezone.now)
-        visibility = models.CharField(max_length=50, choices=visibility_choices, default='public')
-        unlisted = models.BooleanField(default=False)
-        likes = models.IntegerField(default=0)
-        comments = models.IntegerField(default=0)
-        host = models.CharField(max_length=50)
-        url = models.URLField(null=True, blank=True, default=None)
-
-
+    post_id = models.UUIDField(primary_key=True, default=generate_id, editable=False, unique=True)
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE,)
+    title = models.CharField(max_length=100)
+    origin_post = models.URLField()
+    source = models.URLField()
+    description = models.CharField(max_length=200)
+    content = models.CharField(max_length=500, null=True, blank=True)
+    content_type = models.CharField(max_length=50, default="text/html", blank=False, null=False)
+    image_content = models.TextField(null=True, blank=True)
+    categories_id = models.JSONField()
+    published_date = models.DateTimeField(default=timezone.now)
+    visibility = models.CharField(max_length=50, choices=visibility_choices, default='public')
+    unlisted = models.BooleanField(default=False)
+    likes = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+    host = models.CharField(max_length=50)
+    url = models.URLField(null=True, blank=True, default=None)
 
 
 ######### Comment #########
 class Comment(models.Model):
-        comment_id = models.CharField(primary_key=True, default=uuid.uuid4, max_length=100, unique=True, editable=False)
-        post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
-        author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
-        content = models.CharField(max_length=500, null=True)
-        image_content = models.TextField(null=True, blank=True)
-        published_date = models.DateTimeField(default=timezone.now)
-        contentType = models.CharField(max_length=50, default="text/html", blank=False, null=False)
-        host = models.CharField(max_length=50)
-        post_author = models.ForeignKey(Author, on_delete=models.CASCADE)
-        url = models.URLField(null=True, blank=True, default=None)
-
+    comment_id = models.UUIDField(primary_key=True, default=generate_id, editable=False, unique=True)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500, null=True)
+    image = models.TextField(null=True, blank=True)
+    published_date = models.DateTimeField(default=timezone.now)
+    content_type = models.CharField(max_length=50, default="text/html", blank=False, null=False)
+    host = models.CharField(max_length=50)
+    post_author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    url = models.URLField(null=True, blank=True, default=None)
 
 
 ######### Like #########
 class Like(models.Model):
-        like_id = models.CharField(primary_key=True, default=uuid.uuid4, max_length=100, unique=True, editable=False)
-        author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
-        post_id = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
-        comment_id = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, blank=True)
-        notification = models.CharField(max_length=100, default="Someone liked your post!")
-        published_date = models.DateTimeField(default=timezone.now)
-        url = models.URLField(null=True, blank=True, default=None)
-
-
+    like_id = models.UUIDField(primary_key=True, default=generate_id, editable=False, unique=True)
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+    comment_id = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, blank=True)
+    notification = models.CharField(max_length=100, default="You received a like")
+    published_date = models.DateTimeField(default=timezone.now)
+    url = models.URLField(null=True, blank=True, default=None)
 
 
 ######### Node #########
 class Node(models.Model):
-        host = models.CharField(primary_key=True, default=HOSTNAME, max_length=100)
-        user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
-        username = models.CharField(max_length=200, null=False)
-        password = models.CharField(max_length=200, null=False)
-        email = models.EmailField(max_length=200, null=False)
-        displayName = models.CharField(max_length=200, null=True)
-        host_url = models.URLField(max_lenght = 200,null=True, blank=True)
-        date_added = models.DateTimeField(auto_now_add=True)
+    host = models.UUIDField(primary_key=True, default=generate_id, editable=False, unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    username = models.CharField(max_length=200, null=False)
+    password = models.CharField(max_length=200, null=False)
+    email = models.EmailField(max_length=200, null=False)
+    display_name = models.CharField(max_length=200, null=True)
+    host_url = models.URLField(max_lenght = 200,null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
-
-
-class sign_up_form(forms.Form):
-    username = forms.CharField(max_length=200, null=False)
-    password = forms.CharField(max_length=200, null=False)
-    email = forms.EmailField(max_length=200, null=False)
-    displayName = forms.CharField(max_length=200, null=True)
-    host_url = forms.URLField(max_lenght = 200,null=True, blank=True)
+    
+# ######### Sign up #########
+# class sign_up_form(forms.Form):
+#     username = forms.CharField(max_length=200, null=False)
+#     password = forms.CharField(max_length=200, null=False)
+#     email = forms.EmailField(max_length=200, null=False)
+#     display_name = forms.CharField(max_length=200, null=True)
+#     host_url = forms.URLField(max_lenght = 200,null=True, blank=True)
 
