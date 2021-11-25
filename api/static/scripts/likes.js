6ded805e25e919e8415e6ce75d7c798292c1a5d1
TheOnlyMiki <jingzeng@ualberta.ca>
2021-11-25 16:11:36 -0700

    implement a basic AJAX of clicked Like SHOW button and Modify some of Like part in HTML

---
+++ api/static/scripts/likes.js
@@ -0,0 +1,30 @@
+// Handler of Like Post SHOW button click event
+$(".myCustom_like_post_show").on("click", function () {
+    var clockedButtonInformation = $(this).attr("data-bs-target");
+    var postID_index = clockedButtonInformation.indexOf("post_");
+    var poster_index = clockedButtonInformation.indexOf("_author_");
+    var postID = clockedButtonInformation.substring(postID_index + 5, poster_index);
+    var poster = clockedButtonInformation.substring(poster_index + 8);
+
+    $.ajax({
+        csrfmiddlewaretoken: '{{ csrf_token }}',
+        url: "api/author/" + poster + "/posts/" + postID + "/likes",
+        type: "GET",
+        success: function(data) {
+            alert(JSON.stringify(data));
+            var count = 0;
+            var html = "";
+
+            for (var like of data) {
+                count += 1;
+            }
+
+            if (count == 0) {
+                html = "<h5>No one liked this post so far, T^T</h5>";
+            }
+
+            $("#like_post_" + postID).append(html);
+        }
+    })
+        
+});
