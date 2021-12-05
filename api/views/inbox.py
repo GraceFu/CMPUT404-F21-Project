dd879401b75f7ee272ea39f5b8f70e4f725d6320
TheOnlyMiki <jingzeng@ualberta.ca>
2021-12-04 19:37:19 -0700

    Fix the bug of NavBar and Addition the basic HTML of inbox button

--- api/views/inbox.py
+++ api/views/inbox.py
@@ -3,14 +3,18 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from api.utils import invalid_user_view
+from api.models import Inbox
 
 @login_required(login_url='login')
-def my_posts_view(request):
+def my_inbox_view(request):
     # Check the user is invalid in view
     if invalid_user_view(request):
         return redirect("login")
 
     content = {}
     content['my_inbox_page'] = True
+
+    inbox = Inbox.objects.get(author=request.user.author)
+    content['items'] = inbox.items[::-1]
 
     return render(request, "inbox.html", content)
